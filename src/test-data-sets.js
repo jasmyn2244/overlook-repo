0818e651374ba0600adbaa536e04df6cc6f0d70c
Jasmyn Hicks <jasmynh44@gmail.com>
2022-01-13 16:23:58 -0700

    Add testfiles and create bookings data set for testing

---
+++ src/test-data-sets.js
@@ -0,0 +1,72 @@
+const bookings = [
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 20,
+    "date": "2022/02/16",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+  "id": "5fwrgu4i7k55hl6ui",
+  "userID": 20,
+  "date": "2022/02/07",
+  "roomNumber": 24,
+  "roomServiceCharges": []
+},
+{
+  "id": "5fwrgu4i7k55hl6yr",
+  "userID": 20,
+  "date": "2022/02/05",
+  "roomNumber": 2,
+  "roomServiceCharges": []
+},
+{
+  "id": "5fwrgu4i7k55hl75n",
+  "userID": 20,
+  "date": "2022/01/18",
+  "roomNumber": 13,
+  "roomServiceCharges": []
+},
+{
+  "id": "5fwrgu4i7k55hl7fv",
+  "userID": 20,
+  "date": "2022/02/16",
+  "roomNumber": 12,
+  "roomServiceCharges": []
+},
+{
+    "id": "5fwrgu4i7k55hl7lm",
+    "userID": 33,
+    "date": "2022/01/21",
+    "roomNumber": 16,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl7lp",
+    "userID": 11,
+    "date": "2022/02/01",
+    "roomNumber": 14,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl7lt",
+    "userID": 5,
+    "date": "2022/01/18",
+    "roomNumber": 25,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl7lu",
+    "userID": 28,
+    "date": "2022/02/09",
+    "roomNumber": 18,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl7ly",
+    "userID": 31,
+    "date": "2022/02/04",
+    "roomNumber": 16,
+    "roomServiceCharges": []
+  },
+]
