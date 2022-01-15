9da3acf959cb59724e582938e31e1db1a5c62e77
Jasmyn Hicks <jasmynh44@gmail.com>
2022-01-15 11:33:47 -0700

    Add imports from api-calls

--- src/scripts.js
+++ src/scripts.js
@@ -3,11 +3,27 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import { fetchCustomer, fetchBookings, fetchRooms } from './api-calls'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 //import './images/turing-logo.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
 
+//QUERY SELECTORS
+const usernameInput = document.querySelector('#usernameInput');
+const passwordInput = document.querySelector('#passwordInput');
+
+
+console.log('single customer', fetchCustomer(20));
+console.log('bookings', fetchBookings());
+console.log('rooms', fetchRooms());
 //console.log ('Is this connected to the correct repo?')
+const authenticateCustomer = () => {
+  if(passwordInput.value === 'overlook2021') {
+    const currentCustomer = customers.find(currentCust => {
+      customer.ids
+    })
+    const customer = new Customer ()
+  }
+}
